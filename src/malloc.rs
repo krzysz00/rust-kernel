b0e469a5632aa7fc6626c3780d50aa42aa76cf90
Krzysztof Drewniak <krzysdrewniak@gmail.com>
2015-04-10 14:05:37 -0500

    Working heap, no modulo

--- src/malloc.rs
+++ src/malloc.rs
@@ -1,6 +1,8 @@
 const HEAP_START: usize = 0x00100000;
 const HEAP_END: usize = 0x00EFFFFF;
 
+use core;
+
 static mut heap_ptr: usize = HEAP_START;
 
 extern {
@@ -10,12 +12,12 @@ extern {
 #[no_mangle]
 pub unsafe extern fn rust_allocate(size: usize, align: usize) -> *mut u8 {
     let mut new_heap_ptr = heap_ptr;
-    if heap_ptr % align != 0 {
-        new_heap_ptr += (align - (heap_ptr % align));
-    }
+    // if heap_ptr % align != 0 {
+    //     new_heap_ptr += (align - (heap_ptr % align));
+    // }
 
     if new_heap_ptr + size >= HEAP_END {
-        0 as *mut u8
+        core::ptr::null_mut()
     }
     else {
         heap_ptr = new_heap_ptr + size;
