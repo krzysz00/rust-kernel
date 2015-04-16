2149afede31cdc0b90072360c2d335c9f94f4ad4
Krzysztof Drewniak <krzysdrewniak@gmail.com>
2015-04-16 13:58:18 -0500

    More reorganization

--- src/interrupts/mod.rs
+++ src/interrupts/mod.rs
@@ -1,4 +1,5 @@
 mod idt;
+mod pic;
 pub use self::idt::{idtDesc};
 
 use machine::{outb, inb};
@@ -7,11 +8,6 @@ use vga;
 
 use core::prelude::*;
 use vga::Color::*;
-
-const PIC1_CMD: u16 = 0x20;
-const PIC1_DATA: u16 = 0x21;
-const PIC2_CMD: u16 = 0xA0;
-const PIC2_DATA: u16 = 0xA1;
 
 #[link(name="asmcode", repr="static")]
 extern {
@@ -47,41 +43,15 @@ pub extern fn page_fault_handler(address: u32, error: u32) {
 pub extern fn kbd_interrupt_handler() {
     let byte = inb(0x60);
     vga::write_string_with_color(4, 30, "Interrupts on!", Pink, Black);
-    outb(PIC1_CMD, 0x20);
-}
-
-fn remap_pic() {
-    let mask1 = inb(PIC1_DATA);
-    let mask2 = inb(PIC2_DATA);
-
-    outb(PIC1_CMD, 0x11);
-    outb(PIC2_CMD, 0x11);
-
-    outb(PIC1_DATA, 0x20);
-    outb(PIC2_DATA, 0x28);
-
-    outb(PIC1_DATA, 0x04);
-    outb(PIC2_DATA, 0x02);
-
-    outb(PIC1_DATA, 0x01);
-    outb(PIC2_DATA, 0x01);
-
-    // Everything is remapped
-    outb(PIC1_DATA, mask1);
-    outb(PIC2_DATA, mask2);
-}
-
-pub fn mask_pic(master_mask: u8, slave_mask: u8) {
-    outb(PIC1_DATA, master_mask);
-    outb(PIC2_DATA, slave_mask);
+    outb(0x20, 0x20);
 }
 
 // Remaps the PIC, masks everything
 pub fn init() {
     idt::init();
 
-    remap_pic();
-    mask_pic(0xfd, 0xff);
+    pic::remap_pic();
+    pic::mask_pic(0xfd, 0xff);
 
     idt::register_interrupt(0x8, double_fault_wrapper);
     idt::register_interrupt(0xD, gpf_wrapper);
