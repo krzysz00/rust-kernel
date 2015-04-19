04a1f496433d1c3fbe6b089037c09c354201d792
Krzysztof Drewniak <krzysdrewniak@gmail.com>
2015-04-19 14:52:07 -0500

    Processor count

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,7 @@ extern crate alloc;
 extern crate collections;
 
 use core::prelude::*;
+use core::atomic::Ordering;
 use alloc::boxed::Box;
 use collections::string::String;
 
@@ -28,6 +29,7 @@ mod acpi;
 mod interrupts;
 mod paging;
 mod malloc;
+mod smp;
 
 use vga::Color::*;
 
@@ -40,36 +42,38 @@ pub use malloc::{rust_allocate, rust_reallocate, rust_reallocate_inplace,
 #[lang="start"]
 #[no_mangle]
 pub fn k_main() {
-    paging::init();
-    interrupts::init_idt();
-    // YOU MAY NOW PAGE FAULT
-    let smp_info = acpi::smp_info();
-    interrupts::init(&(*smp_info).io_apics);
+    if smp::processor_count.fetch_add(1, Ordering::SeqCst) == 0 {
+        paging::init();
+        interrupts::init_idt();
+        // YOU MAY NOW PAGE FAULT
+        let smp_info = acpi::smp_info();
+        interrupts::init(&(*smp_info).io_apics);
 
-    let greet = "Hello from bare-bones Rust";
+        let greet = "Hello from bare-bones Rust";
 
-    for i in 0..vga::ROWS {
-        for j in 0..vga::COLS {
+        for i in 0..vga::ROWS {
+            for j in 0..vga::COLS {
             vga::write_char_with_color(i, j, ' ', White, LightBlue);
+            }
         }
+        vga::paint_color(0, 0, 30, White, LightGreen);
+        vga::write_string(10, 5, greet);
+        vga::move_cursor(10, 5 + greet.len());
+        vga::write_string_with_color(11, 10, "Test", Black, LightRed);
+
+        let heap = Box::new('H');
+        let heap2 = Box::new('!');
+        vga::write_char(15,4,*heap);
+        vga::write_char_with_color(15, 5, *heap2, LightGray, Pink);
+
+        let mut string = String::from_str("Hello, ");
+        string.push_str("paging");
+        vga::write_string(3,5,&string);
+        unsafe { *(0xB0_00_00_01 as *mut u32) = 0xcafecafe; }
+        unsafe { *(0xA0_00_10_00 as *mut mmu::Descriptor) = gdt::gdt_get(1); }
+        let processors = smp::processor_count.load(Ordering::SeqCst) as u8 + '0' as u8;
+        vga::write_char(4, 4, processors as char);
     }
-    vga::paint_color(0, 0, 30, White, LightGreen);
-    vga::write_string(10, 5, greet);
-    vga::move_cursor(10, 5 + greet.len());
-    vga::write_string_with_color(11, 10, "Test", Black, LightRed);
-
-    let heap = Box::new('H');
-    let heap2 = Box::new('!');
-    vga::write_char(15,4,*heap);
-    vga::write_char_with_color(15, 5, *heap2, LightGray, Pink);
-
-    let mut string = String::from_str("Hello, ");
-    string.push_str("paging");
-    vga::write_string(3,5,&string);
-    unsafe { *(0xB0_00_00_01 as *mut u32) = 0xcafecafe; }
-    unsafe { *(0xA0_00_10_00 as *mut mmu::Descriptor) = gdt::gdt_get(1); }
-    let processors = (*smp_info).processors.len() as u8 + ('0' as u8);
-    vga::write_char(4, 4, processors as char);
 }
 
 #[lang = "stack_exhausted"] extern fn stack_exhausted() {}
